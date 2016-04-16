deae5d08617ba6958427be6c46bfb9bea83cfb19
zhuoyikang <zhuoyikang@gmail.com>
2016-04-16 18:54:47 +0800

    cmt

--- myzshrc.sh
+++ myzshrc.sh
@@ -1,4 +1,5 @@
-# 各种别名
+## 各种别名
+
 alias ws="cd ~/source"
 
 # git 相关别名
@@ -6,5 +7,7 @@ alias gts="git status"
 alias gtc="git commit"
 alias gpso="git push origin"
 
-# 各种环境变量.
-export KERL_CONFIGURE_OPTIONS=--open-ssl=/usr/local/opt/openssl
+## 各种环境变量
+
+# openssl路径，通过homebrew安装
+export KERL_CONFIGURE_OPTIONS=--with-ssl=/usr/local/opt/openssl

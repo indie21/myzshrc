26f08bd5e5491ec03ac5f9c5f6a62a3d017155cb
zhuoyikang <zhuoyikang@gmail.com>
2016-04-16 18:48:45 +0800

    first commit

---
+++ init-my-mac.sh
@@ -0,0 +1,5 @@
+# 安装homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# 安装openssl - for erlang
+brew install openssl
---
+++ myzshrc.sh
@@ -0,0 +1,9 @@
+# 各种别名
+alias ws="cd ~/source"
+
+# git 相关别名
+alias gts="git status"
+alias gtc="git commit"
+
+# 各种环境变量.
+export KERL_CONFIGURE_OPTIONS=--open-ssl=/usr/local/opt/openssl

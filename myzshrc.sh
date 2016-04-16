aca28b7b9e1a1ef6ef391679067048abf82f355c
zhuoyikang <zhuoyikang@gmail.com>
2016-04-16 19:16:37 +0800

    x

--- myzshrc.sh
+++ myzshrc.sh
@@ -7,7 +7,14 @@ alias gts="git status"
 alias gtc="git commit"
 alias gpso="git push origin"
 
+# erlang相关别名
+alias r18=". /Users/zhuoyikang/source/erlang/18.3/activate"
+
 ## 各种环境变量
 
 # openssl路径，通过homebrew安装
 export KERL_CONFIGURE_OPTIONS=--with-ssl=/usr/local/opt/openssl
+
+
+## 启动设置
+r18

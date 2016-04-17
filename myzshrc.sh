7ed8ebaf5854b3107070dd19e53bee71f905a13c
zhuoyikang <zhuoyikang@gmail.com>
2016-04-17 10:27:19 +0800

    erlang

--- myzshrc.sh
+++ myzshrc.sh
@@ -1,20 +1,26 @@
-## 各种别名
+## -------  各种别名
 
 alias ws="cd ~/source"
 
 # git 相关别名
 alias gts="git status"
 alias gtc="git commit"
+alias gto="git checkout"
 alias gpso="git push origin"
 
 # erlang相关别名
 alias r18=". /Users/zhuoyikang/source/erlang/18.3/activate"
+alias develop="erl -pa ebin/ deps/*/ebin -name develop@127.0.0.1  -setcookie abc -detached"
+alias kill_develop="ps -ef |grep develop@127.0.0.1 |grep beam | awk '{print $2}' | xargs kill"
 
-## 各种环境变量
+# 云主机
+alias at1="ssh ubuntu@121.43.59.144"
+
+## ------- 各种环境变量
 
 # openssl路径，通过homebrew安装
 export KERL_CONFIGURE_OPTIONS=--with-ssl=/usr/local/opt/openssl
 
 
-## 启动设置
+## -------  启动设置
 r18
